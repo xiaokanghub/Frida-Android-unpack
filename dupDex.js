ac91a099b772e66db3d5b5f24e7891c5eba7e064
xkang <33888474+xiaokanghub@users.noreply.github.com>
2019-12-03 17:14:15 +0800

    Update dupDex.js

--- dupDex.js
+++ dupDex.js
@@ -11,17 +11,21 @@ m.forEach(function(m){
 
 		moduleFuncName = m.name;
 		console.log("module function name: "+ m.name);
+}else if(m.name.indexOf("OpenMemory") != -1){
+		moduleFuncName = m.name;
+		console.log("module function name: "+ m.name);
+
 };
 
 });
 
-var moduleO = "_ZN3art7DexFile10OpenCommonEPKhjRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_PNS0_12VerifyResultE";
-var moduleP = "_ZN3art13DexFileLoader10OpenCommonEPKhmS2_mRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE";
+//var moduleO = "_ZN3art7DexFile10OpenCommonEPKhjRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_PNS0_12VerifyResultE";
+//var moduleP = "_ZN3art13DexFileLoader10OpenCommonEPKhmS2_mRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE";
 
-if(moduleO == moduleFuncName || moduleP == moduleFuncName){
+if(moduleFuncName != null){
 	console.log("=========================================");
 	console.log("Hook Start");
-	var OpenCommon = Module.findExportByName("libart.so","_ZN3art7DexFile10OpenCommonEPKhjRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_PNS0_12VerifyResultE");
+	var OpenCommon = Module.findExportByName("libart.so",moduleFuncName);
 	if(OpenCommon != undefined){
 		Interceptor.attach(OpenCommon,{
 		onEnter: function(args){
@@ -42,7 +46,7 @@ if(moduleO == moduleFuncName || moduleP == moduleFuncName){
 			var address = parseInt(begin,16) + 0x20;
 			var dex_size = Memory.readInt(ptr(address));
 			console.log("dex_size :" + dex_size);
-			var file = new File("/sdcard/" + dex_size + ".dex", "wb");
+			var file = new File("/data/data/com.jjwxc.reader/" + dex_size + ".dex", "wb");
 			file.write(Memory.readByteArray(begin, dex_size));
 			file.flush();
 			file.close();
@@ -61,5 +65,5 @@ if(moduleO == moduleFuncName || moduleP == moduleFuncName){
 
 
 }else{
-	console.log("Function not exist!\n","Please modify the function name");
+	console.log("Function not exist!\n");
 }
